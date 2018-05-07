bbe5648c369caa03ddb847fbb2dc12d0f5830de3
Keshav Nair <keshavnair101@gmail.com>
2018-05-08 03:58:03 +0530

    fixed set_access_token incorrect typo

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys, tweepy, json
 auth = tweepy.OAuthHandler(consumer_key='V987DRZmMDTuGv8qXwpPDQ',
 	consumer_secret='YRyMcRI9xjaGb6UG5B8WC8pMY0G5ATZ5n1ObO7uAjjo')
 
-auth.set_Access_token(access_token_key='869223589-3zpYLDRAsuz6RgLqUmvdGFDrmDLGVg12XBRqZC4J',
-	access_token_secret='ezkHcbBYfcmfH4Y9T6zhH4FHVxRQZhMsfiW1tFJZ0')
+auth.set_access_token('869223589-3zpYLDRAsuz6RgLqUmvdGFDrmDLGVg12XBRqZC4J',
+	'ezkHcbBYfcmfH4Y9T6zhH4FHVxRQZhMsfiW1tFJZ0')
 
 api = tweepy.API(auth)
 
