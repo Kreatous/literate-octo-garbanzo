59091ad59382d0a72669788fa2e181b6f8687879
Kreatous <kreatous@live.com>
2018-10-27 05:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def word_feats(words):
 
 if __name__ == "__main__":
 
-	auth = tweepy.OAuthHandler(consumer_key='V987DRZmMDTuGv8qXwpPDQ',
-		consumer_secret='YRyMcRI9xjaGb6UG5B8WC8pMY0G5ATZ5n1ObO7uAjjo')
+	auth = tweepy.OAuthHandler(consumer_key='',
+		consumer_secret='')
 
-	auth.set_access_token('869223589-3zpYLDRAsuz6RgLqUmvdGFDrmDLGVg12XBRqZC4J',
-		'ezkHcbBYfcmfH4Y9T6zhH4FHVxRQZhMsfiW1tFJZ0')
+	auth.set_access_token('',
+		'')
 
 	api = tweepy.API(auth)
 
